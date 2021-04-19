c1c41bd1ff1d7fb894588a510a9ac21863da4561
FarhanNitrogen <farhan.masud@selise.ch>
2021-04-20 03:13:34 +0600

    refactred

--- app.js
+++ app.js
@@ -1,24 +1,33 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const server = require('./src/server/server');
-const UserLogin = require('./src/Api/Login/login');
+const mongoose = require('mongoose');
+const login = require('./src/Api/Login/login');
+
+
 const app = express();
-
-
 app.use(express.urlencoded());
 
 const cors = require('cors');
-
 app.use(cors());
 app.use(bodyParser.json());
 
 
-//this is api calls
-const login = new UserLogin(app);
-login.signIn();
-login.refreshToken();
+//this is api call
+app.post('/login', login.Login);
+app.post('/refresh-token', login.RefreshToken);
 
 
 // this is the server
-const connection = new server(8081, app);
-connection.core();
+mongoose
+    .connect(`mongodb+srv://Farhan26:Sherlock1234@farhan01.yzaff.mongodb.net/graphql_node?retryWrites=true&w=majority`,
+        {useNewUrlParser: true, useUnifiedTopology: true}
+    )
+    .then(() => {
+        app.listen(3000, (error) => {
+            if (error) console.log(error);
+            else console.log(`http://localhost:3000`);
+        });
+    })
+    .catch(error => {
+        console.log(error);
+    });
